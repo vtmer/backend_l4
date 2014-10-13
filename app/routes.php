dfcc6a0cc88e5b1f710a5d57eaa638c54f0f24d2
Yannyezixin <646840080@qq.com>
2014-10-13 15:43:18 +0800

    用户 路由配置

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,85 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+/*
+|--------------------------------------------------------------------
+| 后台公开路由
+|--------------------------------------------------------------------
+*/
+Route::get('/backend/login', array(
+    'as' => 'BackendGetLogin',
+    'uses' => 'Controllers\Backend\AdminController@getLogin'
+));
+Route::post('/backend/login', array(
+    'as' => 'BackendPostLogin',
+    'uses' => 'Controllers\Backend\AdminController@postLogin'
+));
+
+/*
+|--------------------------------------------------------------------
+| 后台管理
+|--------------------------------------------------------------------
+*/
+Route::group(array('prefix' => 'backend', 'before'=> 'auth'), function () {
+
+    #后台首页
+    Route::get('/', array(
+        'as' => 'BackendGetIndex',
+        'uses' => 'Controllers\Backend\IndexController@getIndex'
+    ));
+
+    # 退出登录
+    Route::get('/logout', array(
+        'as' => 'BackendLogout',
+        'uses' => 'Controllers\Backend\AdminController@getLogout'
+    ));
+
+    #用户管理
+    Route::group(array('prefix' => 'users', 'before' => ''), function () {
+
+        # 用户管理首页
+        Route::get('/', array(
+            'as' => 'BackendUsersGetIndex',
+            'uses' => 'Controllers\Backend\AdminController@getIndex'
+        ));
+
+        # 个人中心
+        Route::get('/{id}', array(
+            'as' => 'BackendUserPersonCenter',
+            'uses' => 'Controllers\Backend\AdminController@getPersonCenter'
+        ));
+
+        # 修改个人信息
+        Route::post('/edit/{id}', array(
+            'as' => 'BackendUserEditDetail',
+            'uses' => 'Controllers\Backend\AdminController@postDetail'
+        ));
+
+        #重置密码
+        Route::post('reset-password', array(
+            'as' => 'BackendUserResetPassword',
+            'uses' => 'Controllers\Backend\AdminController@postPassword'
+        ));
+
+        # 注册用户
+        Route::get('/signup', array(
+            'as' => 'BackendUserGetSignup',
+            'uses'=> 'Controllers\Backend\AdminController@getSignup'
+        ));
+        Route::post('/signup', array(
+            'as' => 'BackendUserPostSignup',
+            'uses' => 'Controllers\Backend\AdminController@postSignup'
+        ));
+
+        # 删除用户
+        Route::get('/delete/{id}', array(
+            'as' => 'BackendUserDelete',
+            'uses' => 'Controllers\Backend\AdminController@postDelete'
+        ));
+    });
+});
+
