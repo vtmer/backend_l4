8008d29cef725cd3a150f3c691ea7ff8d4ac4d6c
Yannyezixin <646840080@qq.com>
2014-10-15 15:36:38 +0800

    添加图片轮播前台位置配置

---
+++ app/config/photoViewPager.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'homepage' => array('name' => '首页轮播', 'code' => 1),
+    'new_top_page' => array('name' => '资讯顶部轮播', 'code' => 2),
+    'new_left_page' => array('name' => '资讯左侧轮播', 'code' => 3),
+);
+
